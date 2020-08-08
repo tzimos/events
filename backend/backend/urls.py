8e5618500c67f7f19099a47a3e8fcf9733db22fd
Panagiotis Tzimos <panagiotis.tzimos@planixs.com>
2020-08-08 01:32:47 +0100

    Updated the whole project's urls in order to include the api urls and the django debug toolbar on development mode.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,6 +1,22 @@
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import (
+    include,
+    path,
+)
+
+api_urlpatterns = [
+    path("events/", include("event.api.urls")),
+]
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include(api_urlpatterns))
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns.insert(0,
+        path("__debug__/", include(debug_toolbar.urls)),
+    )
