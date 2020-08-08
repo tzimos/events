18c0d9a57fb01479162caea633a9d49207f085c9
Panagiotis Tzimos <panagiotis.tzimos@planixs.com>
2020-08-08 01:27:51 +0100

    Updated the event admin in order to include validation using a custom form.

--- backend/event/admin/event.py
+++ backend/event/admin/event.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
+from event.admin.forms import EventAdminForm
 from event.models import Event
 
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
     """The event admin class for django admin panel."""
+    form = EventAdminForm
