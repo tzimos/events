79140d52ca0906f5a370f4fc33a1d2b342aa60b4
Panagiotis Tzimos <panagiotis.tzimos@planixs.com>
2020-08-09 18:02:00 +0100

    Updated config.js to include the resources urls and the routePaths for SPA navigation.

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,7 +2,16 @@ export const config = {
   HOST: process.env.HOST,
   api: process.env.API_BASE_URL,
   JWToken: "JWToken",
+  JWTokenRefreshKey: "JWTokenRefreshKey",
+  pageTitle: {
+    login: "Login",
+  },
+  routePath: {
+    login: "/",
+    events: "/events"
+  },
   resources: {
-    login: ""
+    getToken: "token/",
+    refreshToken: "token/refresh/",
   }
 }
