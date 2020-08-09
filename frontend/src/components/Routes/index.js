be206755319ba80764a8ea50071041b92f8b8f71
Panagiotis Tzimos <panagiotis.tzimos@planixs.com>
2020-08-09 18:01:01 +0100

    Routes compoent updated with EventsView Component

--- frontend/src/components/Routes/index.js
+++ frontend/src/components/Routes/index.js
@@ -4,13 +4,15 @@ import {Route} from "react-router-dom";
 import {LastLocationProvider} from "react-router-last-location";
 
 import Switch from "react-router-transition-switch";
-import {routePath} from "../../lib/urlPath";
+import {config} from "../../config";
 import LoginView from "../../views/LoginView";
+import EventsView from "../../views/Events";
 
 
 class Routes extends React.PureComponent {
 
   render() {
+    const {routePath} = config;
     return (
       <React.Fragment>
         <LastLocationProvider>
@@ -19,6 +21,10 @@ class Routes extends React.PureComponent {
               exact
               path={routePath.login}
               component={LoginView}/>
+            <Route
+              exact
+              path={routePath.events}
+              component={EventsView}/>
           </Switch>
         </LastLocationProvider>
       </React.Fragment>
