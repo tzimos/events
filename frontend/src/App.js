a6f9f2ada0d8a2e0627835f1aa9dd886ba8915be
Panagiotis Tzimos <panagiotis.tzimos@planixs.com>
2020-08-09 14:42:34 +0100

    Updated App.js to follow the material ui design.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,69 @@
 import React from "react";
 import {Router} from "react-router-dom";
+import {
+  createMuiTheme, MuiThemeProvider,
+  responsiveFontSizes,
+  CssBaseline
+} from "@material-ui/core";
+import {jssPreset} from "@material-ui/styles";
+import globalPlugin from "jss-global";
+
+import camelCase from "jss-plugin-camel-case";
+import {create} from "jss";
+import {
+  createGenerateClassName,
+  JssProvider
+} from "react-jss";
+
 import Routes from "./components/Routes";
 import history from "./lib/history";
+import Loading from "./components/Loading";
+
+let theme = createMuiTheme({
+  palette: {
+    primary: {
+      "main": "#391181",
+    },
+    secondary: {
+      "main": "#FA0764"
+    }
+  },
+  typography: {
+    useNextVariants: true,
+  },
+  direction: "ltr"
+});
+
+
+theme = responsiveFontSizes(theme);
+
+const jss = create({
+  plugins: [
+    ...jssPreset().plugins,
+  ]
+});
+
+jss.use(globalPlugin(), camelCase());
+const generateClassName = createGenerateClassName();
+
 
 class App extends React.PureComponent {
   render() {
     return (
       <React.Fragment>
-        <Router history={history}>
-          <Routes/>
-        </Router>
+        <JssProvider
+          jss={jss}
+          generateClassName={generateClassName}
+        >
+          <MuiThemeProvider
+            theme={theme}>
+            <CssBaseline/>
+            <Router history={history}>
+              <Routes/>
+            </Router>
+            <Loading/>
+          </MuiThemeProvider>
+        </JssProvider>
       </React.Fragment>
     )
   }
