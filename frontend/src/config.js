7d501bad9940f05e3dac172657a62332b20cd37f
Panagiotis Tzimos <panagiotis.tzimos@planixs.com>
2020-08-11 15:27:43 +0100

    Updated config to include new resources

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,17 +1,26 @@
 export const config = {
   HOST: process.env.HOST,
   api: process.env.API_BASE_URL,
+  PROXY_HOST: process.env.PROXY_HOST,
   JWToken: "JWToken",
   JWTokenRefreshKey: "JWTokenRefreshKey",
   pageTitle: {
     login: "Login",
+    events: "Events",
+    eventTickets: "Tickets",
   },
   routePath: {
     login: "/",
-    events: "/events"
+    events: "/events",
+    eventTickets: "/event-tickets"
   },
   resources: {
     getToken: "token/",
     refreshToken: "token/refresh/",
+    eventsList: "events/",
+    createEvent: "events/",
+    eventTickets: "tickets/event/{}/",
+    redeemTicket: "tickets/redeem/{}/",
+    downloadNonRedeemedTickets: "filehandler/export/non-redeemed-tickets/",
   }
 }
