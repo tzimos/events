48e764eee1f197c80cbd56554d3eba613af26f99
Panagiotis Tzimos <panagiotis.tzimos@planixs.com>
2020-08-11 15:13:36 +0100

    Updated Events admin.

--- backend/event/admin/event.py
+++ backend/event/admin/event.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 
-from event.admin.forms import EventAdminForm
 from event.models import Event
 
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
     """The event admin class for django admin panel."""
-    form = EventAdminForm
