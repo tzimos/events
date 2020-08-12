7738312ffdcc3da08b9c5fd471dadf8f3a81c2b8
Panagiotis Tzimos <panagiotis.tzimos@planixs.com>
2020-08-12 01:45:36 +0100

    Updated FE dependencies added a node server to run the FE in the container

--- frontend/webpack/webpack.prod.config.js
+++ frontend/webpack/webpack.prod.config.js
@@ -2,20 +2,26 @@ const path = require("path");
 const base = require("./webpack.base.config");
 const {merge} = require("webpack-merge");
 const Dotenv = require("dotenv-webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 
 const dotEnvPlugin = new Dotenv({
-  path: "../envs/.env.prod"
+  path: path.resolve(__dirname, "../envs/.env.prod")
 });
+
+const htmlPlugin = new HtmlWebpackPlugin({
+      template: path.join(__dirname, "../src/index.html"),
+    })
 
 module.exports = merge(base, {
   plugins: [
     dotEnvPlugin,
+    htmlPlugin,
   ],
   mode: "production",
   output: {
     path: path.resolve(__dirname, "../build"),
-    filename: "app.js.gz"
+    filename: "app.js"
   },
   devtool: "none"
 });
